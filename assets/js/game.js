6b42bafd2338ca986bd8864961aae474fb0a0883
Dayton Mills <dayton@statscraft.net>
2017-08-19 16:09:47 -0700

    Basic state changing

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,14 +4,19 @@
         Main game logic
 
 ***************************************/
+
+$("body").prepend("<div class='container game'></div>");
 gameInit();
 
 function gameInit()
 {
-    //Load Ships
+    //Load selection state
+    $(".game").append("<div class='fleet'><div class='row'><h2>Select Ship: </h2></div><div class='row shipsFleet'></div></div>");
+
+    //Load ships into state
     for(var ship in ships)
     {
-        $(".characters").append("<div class='col-lg-3 ship' id='ship"+ships[ship].id+"'><p class='ship-name'>"+ships[ship].name+"</p><img class='ship-img' src='"+ships[ship].sprites[99]+"'/><p class='ship-health'>"+ships[ship].health+"</p></div>");
+        $(".shipsFleet").append("<div class='col-lg-3 ship' id='ship"+ships[ship].id+"'><p class='ship-name'>"+ships[ship].name+"</p><img class='ship-img' src='"+ships[ship].sprites[99]+"'/><p class='ship-health'>"+ships[ship].health+"</p></div>");
     };
 
     var selected = {
@@ -42,16 +47,29 @@ function gameInit()
                 $("#ship"+enemyShip.id).addClass("enemy");
                 gameRun(myShip, enemyShip);
             }
-            else
-            {
-                console.log("You cant pick that!");
-            }
         })
     }
 }
 
 function gameRun(myShip, enemyShip)
 {
+
+    setBattlefield();
+
+    function setBattlefield()
+    {
+        //Load Arena State
+        $(".fleet").empty();
+        $(".game").append("<div class='arena'><div class='row'><h2>Arena: </h2></div><div class='row shipsArena'></div></div>");
+
+        //Load ships into state
+        var characters = [myShip, enemyShip];
+        for(i = 0; i < characters.length; i++)
+        {
+            $(".shipsArena").append("<div class='col-lg-4 ship' id='ship"+characters[i].id+"'><p class='ship-name'>"+characters[i].name+"</p><img class='ship-img' src='"+characters[i].sprites[99]+"'/><p class='ship-health'>"+characters[i].health+"</p></div>");
+        }
+    }
+
     console.log("Your ship: " + myShip.name);
     console.log("Enemy ship: " + enemyShip.name);
 }
