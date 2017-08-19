4c29c0d220a72479f04b73b9698ab52f8dd1744d
Dayton Mills <dayton@statscraft.net>
2017-08-19 15:37:52 -0700

    Added Enemy Ship Selection

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,26 +14,44 @@ function gameInit()
         $(".characters").append("<div class='col-lg-3 ship' id='ship"+ships[ship].id+"'><p class='ship-name'>"+ships[ship].name+"</p><img class='ship-img' src='"+ships[ship].sprites[99]+"'/><p class='ship-health'>"+ships[ship].health+"</p></div>");
     };
 
-    var shipSelected = false;
+    var selected = {
+        mine: false,
+        enemy: false
+    }
 
     //Ship Selection
     $(".ship").click(function(event) {
-        if(!shipSelected)
+        if(!selected.mine)
         {
-            selectShip(event.target.id);
+            const myShip = ships[event.target.id];
+            myShip.selected = true;
+            selected.mine = true;
+            $("#ship"+myShip.id).addClass("selected");
+            selectEnemy(myShip);
         }
     })
 
-    function selectShip(ship)
+    function selectEnemy(myShip)
     {
-        ships[ship].selected = true;
-        shipSelected = true;
-        console.log(ships[ship]);
-        $("#ship"+ships[ship].id).addClass("selected");
+        $(".ship").click(function(event) {
+            if(!selected.enemy && ships[event.target.id] != myShip)
+            {
+                const enemyShip = ships[event.target.id];
+                enemyShip.enemy = true;
+                selected.enemy = true;
+                $("#ship"+enemyShip.id).addClass("enemy");
+                gameRun(myShip, enemyShip);
+            }
+            else
+            {
+                console.log("You cant pick that!");
+            }
+        })
     }
 }
 
-function gameRun()
+function gameRun(myShip, enemyShip)
 {
-
+    console.log("Your ship: " + myShip.name);
+    console.log("Enemy ship: " + enemyShip.name);
 }
